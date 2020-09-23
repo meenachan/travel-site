76b162e1a11e0869ce787fb6e8ee801feb7538c0
meena <mcquityppie3@gmail.com>
2020-09-23 15:47:19 +0530

    add clients area feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,14 @@ import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import ClientArea from './modules/ClientArea';
 
 
 new MobileMenu();
  new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
  new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
  new StickyHeader();
+ new ClientArea();
 let modal;
 
 document.querySelectorAll(".open-modal").forEach(el => el.addEventListener("click",(e)=>{
